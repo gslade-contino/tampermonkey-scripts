e61b8d09200eff0f2968bdc6cceaedf4c90473aa
Gareth Slade <gareth.slade@contino.io>
2021-02-01 13:07:11 +0000

    Initial commit

---
+++ fix-office-nav.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Fix Office Navigation
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Fix apps navigation to see all labels
+// @author       Gareth Slade
+// @match        https://www.office.com/*
+// @grant        none
+// ==/UserScript==
+
+window.onload = function() {
+    let allAppsButton = document.querySelector('#O365_MainLink_NavMenu');
+    let observer = new MutationObserver(callback);
+    let observerOptions = {
+        childList: true,
+        attributes: true,
+        characterData: false,
+        subtree: true
+    };
+
+
+    function callback (mutations) {
+        const elems = document.querySelectorAll('._1LmT2pyh06k_FxmBeyjKZG');
+
+        for (var i = 0; i < elems.length; i++) {
+            elems[i].setAttribute("style", "width: 100% !important");
+        };
+    }
+
+    observer.observe(allAppsButton, observerOptions);
+};
